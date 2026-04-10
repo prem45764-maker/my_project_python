1c75215c8dc8d1e1034b4912481f05df16307153
prem45764-maker <prem45764@gmail.com>
2026-04-10 09:45:19 +0530

    Add files via upload

---
+++ project_snake_game.py
@@ -0,0 +1,36 @@
+# import random
+# computer = random.choice([0 , -1 , 1])
+# youstr = input("enter your choice:")
+# youdict = {"s": 1 , "w": -1 , "g": 0}
+# reversedict = {1 : "snake" , -1 : "water" , 0 : "gun"}
+# you = youdict[youstr]
+# print(f"you chose {reversedict[you]}\ncomputer chose {reversedict[computer]}")
+
+# if computer == you:
+#     print("its a draw")
+# else:
+#     if (computer - you) == -1 or (computer - you) == 2:
+#         print("you lose!")
+#     else:
+#         print("you win!")
+    
+#     if computer == -1 and you == 1: -2
+#         print("you win!")
+#     elif computer == -1 and you == 0: -1
+#         print("you lose!")
+#     elif computer == 1 and you == -1: 2
+#         print("you lose!")
+#     elif computer == 1 and you == 0: 1
+#         print("you win!")
+#     elif computer == 0 and you == -1: 1
+#         print("you win")
+#     elif computer == 0 and you == 1: -1
+#         print("you lose!")
+#     else:
+#         print("something is wrong")
+# if (computer - you) == -1 or (computer - you) == 2:
+#     print("you lose1")
+# else:
+#     print("you win!")
+        
+    
